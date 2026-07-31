e4d6a73f8ab68d1c7395a24dc096a60b7387496b
aayushpiplani8-dotcom <aayushpiplani8@gmail.com>
2026-07-31 11:42:11 +0530

    File updated at 7/31/2026, 11:42:11 AM

--- DP/1-TwoSum/1-TwoSum.py
+++ DP/1-TwoSum/1-TwoSum.py
@@ -1,9 +1,9 @@
-# Last updated: 7/31/2026, 1:08:14 AM
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        for i in range(n - 1):
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return []  # No solution found
+# Last updated: 7/31/2026, 11:42:11 AM
+1class Solution:
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        n = len(nums)
+4        for i in range(n - 1):
+5            for j in range(i + 1, n):
+6                if nums[i] + nums[j] == target:
+7                    return [i, j]
+8        return []  # No solution found
