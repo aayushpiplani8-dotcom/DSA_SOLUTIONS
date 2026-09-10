3952d35761ba8d73414267cc031d68db4cb5ffe7
aayushpiplani8-dotcom <aayushpiplani8@gmail.com>
2026-09-10 22:12:59 +0530

    Create 46-Permutations

---
+++ Arrays/46-Permutations/46-Permutations.py
@@ -0,0 +1,16 @@
+# Last updated: 9/10/2026, 10:13:05 PM
+1class Solution:
+2    def permute(self, nums):
+3        res = []
+4        
+5        def perms(i):
+6            if i == len(nums):
+7                res.append(nums[:])
+8                return
+9            for j in range(i, len(nums)):
+10                nums[i], nums[j] = nums[j], nums[i]
+11                perms(i + 1)
+12                nums[i], nums[j] = nums[j], nums[i]
+13        
+14        perms(0)
+15        return res
